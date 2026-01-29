dab5e83fa0143b5249ea591541017fdb81b207c6
Your Name <you@example.com>
2026-01-30 00:19:27 +0700

    add config

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static/images')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+MAX_CONTENT_LENGTH = 2 * 1024 * 1024  # 2MB
+
